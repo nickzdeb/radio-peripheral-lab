8a1eff336c0cd32301d3773183db7d069852a4cd
nickzdeb <nickzdeb@gmail.com>
2025-08-04 21:14:53 -0400

    Correcting vivado command call

--- make_project.sh
+++ make_project.sh
@@ -1,9 +1,9 @@
 # the first line constructs the vivado project, but doesn't synthesize or place+route
-#vivado -nolog -nojournal -mode batch -source ./tcl/make_project.tcl
-/tools/Xilinx/Vivado/2024.2/bin/vivado -nolog -nojournal -mode batch -source ./tcl/make_project.tcl
+vivado -nolog -nojournal -mode batch -source ./tcl/make_project.tcl
+#/tools/Xilinx/Vivado/2024.2/bin/vivado -nolog -nojournal -mode batch -source ./tcl/make_project.tcl
 # this next command runs the TCL script that makes the bitfile in vivado and exports the XSA
-#vivado -nolog -nojournal -mode batch -source ./tcl/impl.tcl
-/tools/Xilinx/Vivado/2024.2/bin/vivado -nolog -nojournal -mode batch -source ./tcl/impl.tcl
+vivado -nolog -nojournal -mode batch -source ./tcl/impl.tcl
+#/tools/Xilinx/Vivado/2024.2/bin/vivado -nolog -nojournal -mode batch -source ./tcl/impl.tcl
 # this next line makes the .bit.bin file from the .bit file
 bootgen -image doug.bif -arch zynq -process_bitstream bin -w on
 
